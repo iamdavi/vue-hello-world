99485651782432bae2be657351863c343c1cfc37
David Otero <domato99.dom@gmail.com>
2020-10-25 16:23:44 +0100

    Añadida vista de articulo

---
+++ src/views/Articulo.vue
@@ -0,0 +1,19 @@
+<template>
+    <Titulo texto="Ruta con parámetros" />
+    <h2>Parámetro: {{ $route.params.id }}</h2>
+</template>
+
+<script>
+import Titulo from '@/components/Titulo'
+
+export default {
+    name: "Articulo",
+    components: {
+        Titulo
+    }
+}
+</script>
+
+<style>
+
+</style>
