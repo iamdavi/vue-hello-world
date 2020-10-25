1440791ba90fff57e5dd6de1db888e187ecb579a
David Otero <domato99.dom@gmail.com>
2020-10-25 15:27:10 +0100

    Añadido vue router a la aplicación

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Titulo texto="Mi banco [props]" />
-  <Cuenta />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import Titulo from './components/Titulo'
-import Cuenta from './components/Cuenta'
-
-export default {
-  name: 'App',
-  components: {
-    Titulo,
-    Cuenta
-  }
-}
-</script>
 
 <style>
 #app {
@@ -24,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
