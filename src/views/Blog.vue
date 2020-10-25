f184a243a70fe0ccee1046cb64fd69defb96fe62
David Otero <domato99.dom@gmail.com>
2020-10-25 16:24:13 +0100

    Consima API de jsonplaceholder

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,14 +1,31 @@
 <template>
     <Titulo texto="Blog" />
+    <button @click="consumirApi">Obtener datos</button>
 </template>
 
 <script>
-import Titulo from "../components/Titulo";
+import Titulo from "@/components/Titulo";
 
 export default {
     name: "Blog",
     components: {
         Titulo
+    },
+    data() {
+        return {
+            posts: []
+        }
+    },
+    methods: {
+        async consumirApi() {
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json()
+                console.log(array)
+            } catch (error) {
+                console.log(error)
+            }
+        }
     }
 }
 </script>
